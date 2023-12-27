e0e50eb3a0325648620c8a81de3487d533bd99ba
Deepak Kheterpal <khetarpal.mca@gmail.com>
2023-12-27 22:58:23 +0530

    removing logs from controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -17,7 +17,6 @@ const getJob = async (req, res) => {
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
-    console.log(req.body)
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({job})
 }
